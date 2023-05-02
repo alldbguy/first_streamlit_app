ab2ab713e59cee85d105457cfdfdb68a6556d344
alldbguy <132370482+alldbguy@users.noreply.github.com>
2023-05-02 11:04:44 -0400

    Update streamlit_app.py
    Finished updating menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,8 @@ streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
 
-streamlit.write('Omega3')
+streamlit.write('Omega 3  & Blueberry Oatmeal')
+
+streamlit.write('Kale, Spinach & Rocket Smoothie')
+
+streamlit.write('Hard-Boiled Free-Range Egg')
